6d74d765028469ba8223d1dd0a33d58e54c75b3a
Jokha <joker2002187@gmail.com>
2025-07-22 19:37:53 +0400

    Task 4: adding library book system

---
+++ Textbook.java
@@ -0,0 +1,28 @@
+public class Textbook extends Book{
+    private String subject;
+    private int edition;
+
+    // Constructor that calls parent constructor
+    public Textbook(String title, String author, int pages, String subject, int edition) {
+        super(title,author,pages);
+        this.subject = subject;
+        this.edition = edition;
+    }
+
+    // Override the displayInfo method
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Subject: "+subject);
+        System.out.println("Edition: "+edition);
+    }
+
+    // Add getter methods for subject and edition
+    public int getEdition() {
+        return edition;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+}
